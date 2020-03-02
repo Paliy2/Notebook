e7b3aa97bc92015694d54df10093fab2b611cdca
Yewgen_Dom <52251850+Paliy2@users.noreply.github.com>
2020-03-02 20:12:25 +0200

    Update command_optin.py

--- command_optin.py
+++ command_optin.py
@@ -11,6 +11,9 @@ if __name__ == '__main__':
     # calling __init__ while creating
     notebook = Notebook()
     # method in use
+    # self represents the instance of the class. 
+    # self is the parameter of class
+    # we can access it by using self.var. Instead of self here should be the name of object
     notebook.new_note('Hello Oles')
     # class function variable, like self,var
     print(isinstance(notebook.notes, list))
